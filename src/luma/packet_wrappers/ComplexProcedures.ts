f4d1f74bf17b5c3de96aa7e0744be076ea290d6f
Anton Petrochenko <petrochenko.ai@ubmail.ru>
2023-04-01 18:09:32 +0600

    Improved real 0.30 support (doesn't really work still)

--- src/luma/packet_wrappers/ComplexProcedures.ts
+++ src/luma/packet_wrappers/ComplexProcedures.ts
@@ -8,24 +8,23 @@ import * as OutgoingPackets from "../packet_wrappers/OutgoingPackets"
  * @param world The world the player has joined
  */
 export async function WorldJoinProcedure(joiningPlayer: UnsafePlayer, world: World) {
+  //Redundancy check: make sure the player object belongs the world we're sending them to
   if (verifyWorldSafe(joiningPlayer, world)) {
-    //Spawn the new player for others
-    world.broadcastNotSelf(joiningPlayer, OutgoingPackets.SpawnPlayer(joiningPlayer.entityId, joiningPlayer.username, joiningPlayer))
-  
+    
     await joiningPlayer.sendPacket(OutgoingPackets.LevelInitialize())
     
-    
+    //Send world data
     const worldChunks = world.packageForSending()
     for (let chunkIdx = 0; chunkIdx < worldChunks.length; chunkIdx++) {
       const dataChunk = worldChunks[chunkIdx]
       await joiningPlayer.sendPacket(OutgoingPackets.LevelDataChunk(dataChunk, chunkIdx, worldChunks.length))
     }
-  
     
-    //Set player's spawn point
-    await joiningPlayer.sendPacket(OutgoingPackets.SpawnPlayer(-1, joiningPlayer.username, joiningPlayer))
-  
+    //Finalize level
     await joiningPlayer.sendPacket(OutgoingPackets.LevelFinalize(world.sizeX, world.sizeY, world.sizeZ))
+    
+    // Set player's own spawn point
+    await joiningPlayer.sendPacket(OutgoingPackets.SpawnPlayer(-1, joiningPlayer.username, joiningPlayer))
   
     //Spawn others for the new player
     world.players.forEach( (existingPlayer) => {
@@ -35,6 +34,10 @@ export async function WorldJoinProcedure(joiningPlayer: UnsafePlayer, world: Wor
       }
     })
     
+    //Spawn the new player for others
+    world.broadcastNotSelf(joiningPlayer, OutgoingPackets.SpawnPlayer(joiningPlayer.entityId, joiningPlayer.username, joiningPlayer))
+    
+    //Finally, announce in chat
     world.broadcast(OutgoingPackets.Message(`&b${joiningPlayer.username} joined the world`))
   }
 }
