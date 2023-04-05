f61d10085d763064f15c51f1eac4ec48797f30d9
Anton Petrochenko <petrochenko.ai@ubmail.ru>
2023-04-06 00:24:44 +0600

    CPE Negotiation. Fix TCP packet coalescing breaking everything. Vastly improve network stability.

--- src/luma/packet_wrappers/IncomingPackets.ts
+++ src/luma/packet_wrappers/IncomingPackets.ts
@@ -2,7 +2,12 @@ import { unpack } from "python-struct"
 import { BlockFractionUnit, BlockUnit, MVec3 } from "../util/Vectors/MVec3"
 import { Orientation } from "../util/Vectors/Orientation"
 
-export class PlayerIdentification{
+//THIS MODULE MAY ONLY CONTAIN INCOMING PACKET HELPER CLASSES
+//ALL CLASSES IN THIS FILE MUST CONTAIN ID AND BYTES STATIC PROPERTIES
+//IT IS LATER USED TO AUTOMATE THE PROCESS OF PACKET IDENTIFICATION
+//TODO: make it so the above is not required. Probably refactor this whole file...
+
+export class PlayerIdentification {
   constructor(
     public username: string,
     public verificationKey: string,
@@ -12,6 +17,8 @@ export class PlayerIdentification{
     const data = unpack('>BB64s64sB', packet) as [number, number, string, string, number]
     return new this(data[2].trimEnd(), data[3].trimEnd(), data[4] == 0x42)
   }
+  static id = 0x00
+  static bytes = 131
 }
 
 export class PositionAndOrientation{
@@ -25,6 +32,9 @@ export class PositionAndOrientation{
     const data = unpack('>BBHHHBB', packet) as [number, number, BlockFractionUnit, BlockFractionUnit, BlockFractionUnit, number, number]
     return new this(data[1], new MVec3<BlockFractionUnit>( data[2], data[3], data[4] ), new Orientation(data[5], data[6])) 
   }
+
+  static id = 0x08
+  static bytes = 10
 }
 
 export class SetBlock{
@@ -38,6 +48,9 @@ export class SetBlock{
     const data = unpack('>BHHHBB', packet) as [number, BlockUnit, BlockUnit, BlockUnit, number, number]
     return new this(new MVec3<BlockUnit>(data[1], data[2], data[3]), data[4] == 1, data[5])
   }
+
+  static id = 0x05
+  static bytes = 9
 }
 
 export class Message {
@@ -49,4 +62,37 @@ export class Message {
     const data = unpack('>BB64s', packet) as [number, number, string]
     return new this(data[2].trimEnd())
   }
+
+  static id = 0x0d
+  static bytes = 66
+}
+
+export class CPE_ExtInfo {
+  constructor (
+    public appName: string,
+    public extensionCount: number
+  ) {}
+
+  static from(packet: Buffer) {
+    const data = unpack('>B64sH', packet) as [number, string, number]
+    return new this(data[1].trimEnd(), data[2])
+  }
+
+  static id = 0x10
+  static bytes = 67
+}
+
+export class CPE_ExtEntry {
+  constructor (
+    public extName: string,
+    public version: number
+  ) {}
+
+  static from(packet: Buffer) {
+    const data = unpack('>B64si', packet) as [number, string, number]
+    return new this(data[1].trimEnd(), data[2])
+  }
+
+  static id = 0x11
+  static bytes = 69
 }
